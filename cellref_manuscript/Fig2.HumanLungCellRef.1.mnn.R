55a1f5093b1ea7d4d988ed62898e7281762e5e1a
Guo <Minzhe.Guo@cchmc.org>
2023-01-14 13:55:03 -0500

    Update code for CellRef manuscript

--- cellref_manuscript/Fig2.HumanLungCellRef.1.mnn.R
+++ cellref_manuscript/Fig2.HumanLungCellRef.1.mnn.R
@@ -17,9 +17,7 @@ obj = readRDS(file=".Data/full.object.rds")
 obj = NormalizeData(obj)
 obj = CellCycleScoring(obj, s.features = Seurat::cc.genes.updated.2019$s.genes, g2m.features = Seurat::cc.genes.updated.2019$g2m.genes)
 
-print(obj)
-
-# batch correction of data from different donors
+# MNN based batch correction of data from different donors
 obj = doDataIntegration(obj, integration.batch ="DonorID",
                         method="Monocle3-mnn",
                         npcs=200, umap.min_dist = 0.1,  do.clustering = T)
